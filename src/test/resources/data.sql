b3ae47d88b09eb257ec50384311a3b4e942d965c
eyaraouine <eyaraouine15@gmail.com>
2024-11-08 17:07:02 +0100

    modfiied smtgh

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO users_table (username, firstName, lastName, userRole, createdAt, updatedAt)  VALUES('ayouta', 'eya', 'raouine', 'admin', '2024-11-05','2023-1-25');
 INSERT INTO users_table (username, firstName, lastName, userRole, createdAt, updatedAt)  VALUES('joseph', 'youcef', 'ouaz', 'admin', '2023-06-25','2023-06-25');
 INSERT INTO users_table (username, firstName, lastName, userRole, createdAt, updatedAt)  VALUES('winx', 'flora', 'cam', 'user', '2023-06-25','2023-06-25');
-INSERT INTO users_table (username, firstName, lastName, userRole, createdAt, updatedAt)  VALUES('vivi', 'vicotoria', 'maurault', 'user', '2023-06-25','2023-06-25');
+INSERT INTO users_table (username, firstName, lastName, userRole, createdAt, updatedAt)  VALUES('viviana', 'vicotoria', 'maurault', 'user', '2023-06-25','2023-06-25');
